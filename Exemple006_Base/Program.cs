fc38fb0f2aecd7db4b9f8fe1b8ada4569403935e
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-24 14:14:04 +0400

    NewFolderExemple006_base

--- Exemple006_Base/Program.cs
+++ Exemple006_Base/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 8;
+int c = 9;
+int d = 3;
+int e = 4;
+
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+if ( d > max ) max = d;
+if ( c > max ) max = c;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
