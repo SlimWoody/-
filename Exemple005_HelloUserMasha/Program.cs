cd378a07b0318faca196503b0cb44e0365ea13d4
Pavel_Luzin <st_dizainer@mail.ru>
2023-02-24 13:57:38 +0400

    AddedNewFoldersExemple005

--- Exemple005_HelloUserMasha/Program.cs
+++ Exemple005_HelloUserMasha/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите Ваше имя: ");
+String username = Console.ReadLine ();
+
+if (username.ToLower()=="маша")
+{
+    Console.WriteLine ("Ура, это же Маша.");
+} 
+
+else
+{
+    Console.WriteLine ("Привет, ");
+    Console.WriteLine (username);
+}
